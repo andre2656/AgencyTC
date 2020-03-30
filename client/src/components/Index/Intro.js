988df0bbff089a84a545b30bc324009141d71341
Andre <andre2656@gmail.com>
2020-03-30 11:59:42 -0600

    Updated structure for buttons on intro.js

--- client/src/components/Index/Intro.js
+++ client/src/components/Index/Intro.js
@@ -3,39 +3,48 @@ import { Link } from 'react-router-dom';
 import './Index.css'
 
 
-class Intro extends Component{
+class Intro extends Component {
     signUp = () => {
         window.location.pathname = "/sign-up";
     }
     render() {
-    return(
-        <div className = "container" style = {{ display: 'block', backgroundColor: '#000', }}>
-    <div className="row" style={{ backgroundColor: '#000', position: 'absolute', right: 0, left: 0, height: 900, marginTop: 20, }}>
-        <div className="col-md-1" />
-        <div className="col-md-6" id='introText'>
-            <h2 className="sharp">Agency Name</h2>
-                    <button className='col-md-2' id='portBtns'>SWIM</button>
-                    <button className='col-md-2' id='portBtns'>SURF</button>
-                    <button className='col-md-2' id='portBtns'>DENIM</button>
-                    <button className='col-md-3' id='portBtns'>PRESS & BLOGGERS</button> <br />
-                    <button className='col-md-3' id='portBtns'>CREATIVE DIRECTION</button> 
-                    <button className='col-md-4' id='portBtns'>SPORTS & ACCESSORIES</button> <br />
-                    <button className='col-md-2' id='portBtns'>TECHNICAL</button>
-                    <button className='col-md-2' id='portBtns'>CURRENT</button>
-                    <button className='col-md-2' id='portBtns'>CONTACT</button>
-            <p style={{ fontSize: 28 }} className='sharp'> </p>
-            <p style={{ fontSize: 22 }} className='sharp'> From $20/mo. or $200 with referral.* </p>
-            <button className='introSignUpBtn' onClick={this.signUp}> Sign Up </button>
-            <button className='learnMore'>Learn more</button>
-        </div>
-        <div className="col-md-4" >
-            <img id='Logo' src="images/TCLogoBlk.jpg" alt='shop' />
-        </div>
-    </div>
-        </div >
-    )
+        return (
+            <div className="container" style={{ display: 'block', backgroundColor: '#000', }}>
+                <div className="row" style={{ backgroundColor: '#000', position: 'absolute', right: 0, left: 0, height: 900, marginTop: 20, }}>
+                    <div className="col-md-1" />
+                    <div className="col-md-6" id='introText'>
+                        <h2 className="sharp">Agency Name</h2>
+                        <div className='container'>
+                            <div className='row'>
+                                <button className='col-md-2' id='portBtns'>SWIM</button>
+                                <button className='col-md-2' id='portBtns'>SURF</button>
+                                <button className='col-md-2' id='portBtns'>DENIM</button>
+                                <button className='col-md-3' id='portBtns'>PRESS & BLOGGERS</button>
+                            </div>
+                            <div className='row'>
+                                <button className='col-md-3' id='portBtns'>CREATIVE DIRECTION</button>
+                                <button className='col-md-4' id='portBtns'>SPORTS & ACCESSORIES</button>
+                            </div>
+                            <div className='row'>
+                                <button className='col-md-2' id='portBtns'>TECHNICAL</button>
+                                <button className='col-md-2' id='portBtns'>CURRENT</button>
+                                <button className='col-md-2' id='portBtns'>CONTACT</button>
+                            </div>
+                        </div>
+
+                        <p style={{ fontSize: 28 }} className='sharp'> </p>
+                        <p style={{ fontSize: 22 }} className='sharp'> From $20/mo. or $200 with referral.* </p>
+                        <button className='introSignUpBtn' onClick={this.signUp}> Sign Up </button>
+                        <button className='learnMore'>Learn more</button>
+                    </div>
+                    <div className="col-md-4" >
+                        <img id='Logo' src="images/TCLogoBlk.jpg" alt='shop' />
+                    </div>
+                </div>
+            </div >
+        )
     }
-    
+
 }
 
 export default Intro;
