03ae9b8928fedcf42404fa85f48477b3104550ee
Andre <andre2656@gmail.com>
2020-03-30 15:29:52 -0600

    deleted template format from portfolio

--- client/src/components/Index/Portfolio.js
+++ client/src/components/Index/Portfolio.js
@@ -5,24 +5,8 @@ import './Index.css'
 
 const Portfolio = () => {
     return (
-        <div className="container" style={{ display: 'block', }}>
-            <div className="row" style={{ backgroundColor: '#0a0a0a', position: 'absolute', right: 50, left: 50, top: 1020, height: 550 }}>
-
-                <div className='col-md-7'>
-                    <img id='marketingImg' src="images/marketingImage.jpg" alt='Sales Img' />
-                </div>
-                <div className="col-md-4" id='introText'>
-                    <h2 className="dark">Marketing & PR</h2>
-                    <p style={{ fontSize: 20 }} className='dark'>
-                        Social Media Development.<br />
-                        Press.
-                        Celebrity Adoption.<br />
-                        Blogger Adoption.
-                    </p>
-                    <button className='learnMore'>Learn more </button>
-                </div>
-
-            </div>
+        <div className="container" style={{ }}>
+            
         </div>
     )
 }
